526ab92db605441f59c224c96204c0407e0d2191
Emika Ijuin <emikaijuin@gmail.com>
2019-03-24 23:13:31 +0800

    set up inputs and pins for multiple joysticks

--- sketch_jan15a/sketch_jan15a.ino
+++ sketch_jan15a/sketch_jan15a.ino
@@ -8,7 +8,7 @@ Servo servo_3;
 Servo servo_4;
 Servo servo_5;
 
-// Declare Servo Pins
+// DECLARE SERVO PINS
 int servo_pin_0 = 2; // hand grab
 int servo_pin_1 = 3;
 int servo_pin_2 = 4;
@@ -16,21 +16,24 @@ int servo_pin_3 = 5;
 int servo_pin_4 = 6; // wrist extension
 int servo_pin_5 = 7; // wrist rotation
 
-// Declare Joystick Input Pins and State
+// DECLARE JOYSTICK PINS AND STATE
 // right hand joystick
 int x_key_0 = A0;
 int y_key_0 = A1;
 // lefthand joystick
-int x_key_1 = A2;
-int y_key_1 = A3;
+// int x_key_1 = A2;
+// int y_key_1 = A3;
 int switchState;
 int switch_pin = 5;
 int prevSwitchState = HIGH;
 
 // Declare positions
-int x_pos;
-int y_pos;
-int pos = 0;
+int x_pos_0;
+int y_pos_0;
+int x_pos_1;
+int y_pos_1;
+int pos_0 = 0;
+int pos_1 = 90;
 
 // Declare increment angle to control speed of movement
 int increment = 2;
@@ -43,7 +46,7 @@ void setup()
   // servo_1.attach(servo_pin_1);
   // servo_2.attach(servo_pin_2);
   // servo_3.attach(servo_pin_3);
-  servo_4.attach(servo_pin_4);
+  // servo_4.attach(servo_pin_4);
   servo_5.attach(servo_pin_5);
 
   // servo_0.write(pos); // Don't override previous position on reboot
@@ -51,6 +54,8 @@ void setup()
   pinMode(y_key_0, INPUT);
   pinMode(switch_pin, INPUT);
   digitalWrite(switch_pin, prevSwitchState);
+  // pinMode(x_key_1, INPUT);
+  // pinMode(y_key_1, INPUT);
 }
 
 void loop()
@@ -58,69 +63,81 @@ void loop()
   // READ JOYSTICK INPUT
 
   switchState = digitalRead(switch_pin);
-  x_pos = analogRead(x_key_0);
-  y_pos = analogRead(y_key_0);
+  x_pos_0 = analogRead(x_key_0);
+  y_pos_0 = analogRead(y_key_0);
+
+  // x_pos_1 = analogRead(x_key_1);
+  // y_pos_1 = analogRead(y_key_1);
 
   // CONTROL HAND CLOSING AND OPENING
-
-  if (switchState == LOW && switchState != prevSwitchState)
-  {
-    if (servo_0.read() < 90)
-    {
-      servo_0.write(170);
-    }
-    else
-    {
-      servo_0.write(0);
-    }
-  }
-
-  prevSwitchState = switchState;
-
-  // Wrist servo can continuously rotate, do not need to set constraints on max angles
-  // if (x_pos < 300)
+  // if (switchState == LOW && switchState != prevSwitchState)
   // {
-  //   if (pos >= 10)
+  //   if (servo_0.read() < 90)
   //   {
-  //     pos = pos - 5;
-  //     servo_5.write(pos);
+  //     servo_0.write(170);
+  //   }
+  //   else
+  //   {
+  //     servo_0.write(0);
   //   }
   // }
+  // prevSwitchState = switchState;
 
-  // if (x_pos > 700)
+  Serial.println(y_pos_0);
+  delay(100);
+  // CONTROL WRIST EXTENSION
+  // if (x_pos_1 < 300)
   // {
-  //   if (pos <= 180)
-  //   {
-  //     pos = pos + 5;
-  //     servo_5.write(pos);
-  //   }
+  // if (pos_1 >= increment)
+  // {
+  //   pos_1 = pos_1 - increment;
+  // }
+  // else
+  // {
+  //   pos_1 = 0;
+  // }
+  // servo_4.write(pos_1);
+  // delay(50);
+  // }
+  // if (x_pos_1 > 700)
+  // {
+  // if (pos_1 <= 180 - increment)
+  // {
+  //   pos_1 = pos_1 + increment;
+  // }
+  // else
+  // {
+  //   pos_1 = 180;
+  // }
+  // servo_4.write(pos_1);
+  // delay(50);
   // }
 
   // CONTROL WRIST ROTATION
-  if (x_pos < 300)
-  {
-    if (pos >= increment)
-    {
-      pos = pos - increment;
-    }
-    else
-    {
-      pos = 0;
-    }
-    servo_5.write(pos);
-    delay(50);
-  }
-  if (x_pos > 700)
-  {
-    if (pos <= 180 - increment)
-    {
-      pos = pos + increment;
-    }
-    else
-    {
-      pos = 180;
-    }
-    servo_5.write(pos);
-    delay(50);
-  }
+  // if (x_pos_0 < 300)
+  // {
+  //   if (pos_0 >= increment)
+  //   {
+  //     pos_0 = pos_0 - increment;
+  //   }
+  //   else
+  //   {
+  //     pos_0 = 0;
+  //   }
+  //   servo_5.write(pos_0);
+  //   delay(50);
+  // }
+  // if (x_pos_0 > 700)
+  // {
+  //   if (pos_0 <= 180 - increment)
+  //   {
+  //     pos_0 = pos_0 + increment;
+  //   }
+  //   else
+  //   {
+  //     pos_0 = 180;
+  //   }
+  //   servo_5.write(pos_0);
+  //   delay(50);
+  // }
 }
